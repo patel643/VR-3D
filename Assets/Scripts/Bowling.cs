a58e43b682dc37c4691a2f4db79a2642e1084675
Nidhi <31937878+patel643@users.noreply.github.com>
2017-11-27 16:56:50 -0600

    first commit

---
+++ Assets/Scripts/Bowling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowling : MonoBehaviour {
+
+    private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < -5f)
+        {
+            transform.position = startPosition;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+	}
+}
---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+
+
+public class ButtonHandler : MonoBehaviour
+{
+
+    //These bools are just so you can easily choose which level you would like to load
+    //Enable only the level you want in the editor
+
+    public bool floor1;
+    public bool floor2;
+    public bool floor3;
+
+
+    void OnTriggerEnter(Collider Other) //Senses when an controller touches the object
+    {
+        if (Other.tag == "VrController")
+        {
+            if (floor1 == true)
+            {
+                SceneManager.LoadScene("Scene1"); //This will load the scene that is named "Floor One" change the scene name in the script
+            }
+            else if (floor2 == true)
+            {
+                SceneManager.LoadScene("Scene2"); // loads a scene called "Floor Two"
+            }
+            else if (floor3 == true)
+            {
+                SceneManager.LoadScene("Scene3");
+            }
+            else
+            {
+                Debug.Log("Multiple bools were enabled on one object");
+            }
+        }
+    }
+
+}
